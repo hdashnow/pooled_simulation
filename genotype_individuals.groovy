47c7df353096c00e16510e1df473b9eee516fa26
Harriet Dashnow <h.dashnow@gmail.com>
2018-10-09 12:45:33 +1100

    genotype individuals that are inputs to simulation and generate gvcfs

--- genotype_individuals.groovy
+++ genotype_individuals.groovy
@@ -1,22 +1,38 @@
 // Bpipe pipeline
 // Harriet Dashnow 19 Feb 2018
-// Takes mapped exome bams and calls variants
+// Takes exome fastqs, aligns them, then does gvcf calling (to be later used for joint calling)
 
 load 'pipeline_config.groovy'
 load 'pipeline_stages.groovy'
 
+// Settings
+ploidy = 2
 EXOME_TARGET="/group/bioi1/shared/genomes/hg19/exome_targets/NIMBLEGENV2.bed"
 
-set_ploidy = {
-    branch.ploidy = 2
+@preserve("*.bam")
+align_bwa = {
+    doc "Align reads with bwa mem algorithm."
+
+    output.dir="align"
+
+    def fastaname = get_fname(REF)
+            //set -o pipefail
+    from('fastq.gz', 'fastq.gz') produce(branch.sample + '.bam') {
+        exec """
+            bwa mem -M -t $threads
+            -R "@RG\\tID:${flowcell}.${lane}\\tPL:$PLATFORM\\tPU:${flowcell}.${lane}.${library}\\tLB:${library}\\tSM:${sample}"
+            $REF
+            $inputs |
+            samtools view -bSuh - | samtools sort -o $output.bam -T $output.bam.prefix
+        """, "bwa"
+    }
 }
 
 run {
     '%_R*.fastq.gz' * [
-        set_fastq_info +
+        set_fastq_info + set_sample_info +
         align_bwa + index_bam +
         dedup +
-        set_ploidy + 
-        call_variants + filter_vcf_qual
-    ]
+        coverage +
+        call_variants_gvcf ]
 }

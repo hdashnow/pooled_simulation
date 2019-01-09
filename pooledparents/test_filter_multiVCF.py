001dc8f4e978683bda5250c9bcac940ac6aa8dd7
Harriet Dashnow <h.dashnow@gmail.com>
2019-01-09 11:12:29 +1100

    test combining read and ploidy filters

--- pooledparents/test_filter_multiVCF.py
+++ pooledparents/test_filter_multiVCF.py
@@ -19,6 +19,15 @@ def test_set_read_filter_ploidy(total_reads_pool, ploidy, tech_variation, expect
     assert set_read_filter(total_reads_pool, filter_reads = None, ploidy = ploidy,
         tech_variation = tech_variation) == expected
 
+@pytest.mark.parametrize("total_reads_pool, filter_reads, ploidy, tech_variation, combine_filters, expected", [
+    (100, 1, 10, 1, "strict", 10),
+    (100, 1, 10, 1, "lenient", 1),
+])
+def test_set_read_filter_both(total_reads_pool, filter_reads, ploidy, tech_variation,
+                            combine_filters, expected):
+    assert set_read_filter(total_reads_pool, filter_reads = filter_reads, ploidy = ploidy,
+        tech_variation = tech_variation, combine_filters = combine_filters) == expected
+
 def test_set_read_filter_wrongargs():
     with pytest.raises(ValueError):
         set_read_filter(10, filter_reads = 1, ploidy = 2, tech_variation = 0.5, combine_filters = 'max')

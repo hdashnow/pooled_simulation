4b8884d4bc79a7504015102da726ae6afbf28da9
Harriet Dashnow <h.dashnow@gmail.com>
2019-02-01 14:41:02 +1100

    end to end test for filter_multiVCF.py

--- pooledparents/test_filter_multiVCF.py
+++ pooledparents/test_filter_multiVCF.py
@@ -1,5 +1,8 @@
 from filter_multiVCF import *
 import pytest
+import glob
+
+#pytest_plugins = ["pytester"]
 
 @pytest.mark.parametrize("total_reads_pool,filter_reads,expected", [
     (100, 10, 10),
@@ -32,3 +35,24 @@ def test_set_read_filter_both(total_reads_pool, filter_reads, ploidy, tech_varia
 def test_set_read_filter_wrongargs():
     with pytest.raises(ValueError):
         set_read_filter(10, filter_reads = 1, ploidy = 2, tech_variation = 0.5, combine_filters = 'max')
+
+@pytest.fixture
+def my_run(testdir, scriptdir = '.'):
+    def do_run(*args, scriptdir):
+        args = ['python', scriptdir+"filter_multiVCF.py"] + list(args)
+        return testdir.run(*args)
+    return do_run
+
+def test_end2end(tmpdir, my_run):
+    dir_this_file = os.path.dirname(os.path.realpath(__file__)) + '/'
+    datadir = dir_this_file + "test_data/"
+    output = "pooled_sim_compare.csv"
+    result = my_run("--in_vcf", datadir+"merge.2.pools_probands.vcf",
+                    "--pool", '2',
+                    "--probands", 'SRR1301698', 'SRR1301861',
+                    "--out_csv", output,
+                    scriptdir = dir_this_file)
+    assert result.ret == 0
+    with open(output, "r") as f:
+        newcontent = f.read()
+

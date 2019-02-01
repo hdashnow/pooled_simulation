5795955864f6ab40b1e525e3f35955301980df74
Harriet Dashnow <h.dashnow@gmail.com>
2019-02-01 16:21:32 +1100

    test get_nonref_alleles

--- pooledparents/test_filtervcf.py
+++ pooledparents/test_filtervcf.py
@@ -6,6 +6,7 @@ def test_variant_id():
 
 @pytest.mark.parametrize("fname, expected", [
     ('test.bam', 'test'),
+    ('merge.10.bam', '10'),
 ])
 def test_sample_id_from_fname(fname, expected):
     assert sample_id_from_fname(fname) == expected
@@ -20,5 +21,12 @@ def test_sample_id_from_fname(fname, expected):
 def test_count_nonref_alleles(GT_string, expected):
     assert count_nonref_alleles(GT_string) == expected
 
-
-
+@pytest.mark.parametrize("GT_string, expected", [
+    ('0/0', set() ), # returns (nonref_allele_count, total_alleles)
+    ('1/1', set('1') ),
+    ('./.', set() ),
+    ('./1', set('1') ),
+    ('0/1/2/2', set(['1','2']) ),
+])
+def test_get_nonref_alleles(GT_string, expected):
+    assert get_nonref_alleles(GT_string) == expected
